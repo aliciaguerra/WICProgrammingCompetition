7b36086b2eb98dd8b1a10c1e04e63304222fce72
Alicia Guerra <aiguerra@ucsd.edu>
2015-04-09 21:31:32 -0700

    Create Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+//We scan in the number of integers in the integer array.
+int intinarray=sc.NextInt();
+//We scan in all the integers in the integer array.
+for(int i=0; i<intinarray-1; i++){
+}
+public static int luckOut(int[] numbers) {
+for(int i=0; i<intinarray-1; i++){
+if(a[i]
+}
+}
